ecded37318c16f0f47662311b62660159501064e
danielkl3 <danielkl@cyber.org.il>
2025-03-18 20:28:17 +0200

    Added empty main fxml window to start the project

--- finance-app/src/main/java/com/kletinich/App.java
+++ finance-app/src/main/java/com/kletinich/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480); // TO DO: add fxml file of main window instead of primary
+        scene = new Scene(loadFXML("MainWindow"), 640, 480); 
         stage.setScene(scene);
         stage.show();
     }
