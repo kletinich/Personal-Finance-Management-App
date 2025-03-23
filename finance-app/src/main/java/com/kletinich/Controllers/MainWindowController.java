16f991ea0e44ebb38fd6a241edce891ab11f54aa
danielkl3 <danielkl@cyber.org.il>
2025-03-23 20:44:45 +0200

    added windows for transactions,budget,savings,statistics. currently they contain just pane and label. can switch between windows with button press

--- finance-app/src/main/java/com/kletinich/Controllers/MainWindowController.java
+++ finance-app/src/main/java/com/kletinich/Controllers/MainWindowController.java
@@ -2,6 +2,7 @@ package com.kletinich.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
 // controller of the main window
@@ -14,24 +15,45 @@ public class MainWindowController {
 
     @FXML private StackPane windowsStackPane;
 
+    @FXML private Pane transactionWindow;
+    @FXML private Pane budgetWindow;
+    @FXML private Pane savingWindow;
+    @FXML private Pane statisticsWindow;
+
+    @FXML
+    public void initialize(){
+        hideAllWindows();
+    }
+
+    private void hideAllWindows(){
+        transactionWindow.setVisible(false);
+        budgetWindow.setVisible(false);
+        savingWindow.setVisible(false);
+        statisticsWindow.setVisible(false);
+    }
+
     @FXML
     public void switchToTransactions(){
-        System.out.println("Switching to transactions");
+        hideAllWindows();
+        transactionWindow.setVisible(true);
     }
 
     @FXML
     public void switchToBudgets(){
-        System.out.println("Switching to budgets");
+        hideAllWindows();
+        budgetWindow.setVisible(true);
     }
 
     @FXML
     public void switchToSavings(){
-        System.out.println("Switching to savings");
+        hideAllWindows();
+        savingWindow.setVisible(true);
     }
 
     @FXML
     public void switchToStatistics(){
-        System.out.println("Switching to statistics");
+        hideAllWindows();
+        statisticsWindow.setVisible(true);
     }
     
     @FXML
