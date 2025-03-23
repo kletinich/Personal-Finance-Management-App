915b518cc216ab506cf4bda5fa094a7210243075
danielkl3 <danielkl@cyber.org.il>
2025-03-23 23:48:27 +0200

    Added controller class for TransactionWindow. Added UI to TransactionWindow

---
+++ finance-app/src/main/java/com/kletinich/Controllers/TransactionWindowController.java
@@ -0,0 +1,36 @@
+package com.kletinich.Controllers;
+
+import java.sql.Timestamp;
+
+import com.kletinich.database.tables.Transaction;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class TransactionWindowController {
+
+    @FXML private Label balanceLabel;
+
+    @FXML private ComboBox<String> typeFilter;
+    @FXML private ComboBox<String> categoryBox;
+    @FXML private TextField amountTextField;
+    @FXML private Button filterButton;
+
+    @FXML private TableView<Transaction> transactionsTable;
+    @FXML private TableColumn<Transaction, Integer> idColumn;
+    @FXML private TableColumn<Transaction, Double> amountColumn;
+    @FXML private TableColumn<Transaction, String> categoryColumn;
+    @FXML private TableColumn<Transaction, Timestamp> dateColumn;
+    @FXML private TableColumn<Transaction, String> budgetSavingColumn;
+    @FXML private TableColumn<Transaction, String> noteColumn;
+    @FXML private TableColumn<Transaction, Button> actionsColumn;
+
+    @FXML private Button newTransactionButton;
+    
+}
