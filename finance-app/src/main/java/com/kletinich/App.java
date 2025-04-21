5302fc48c4d424044fc7c23472098c90cf37fe4e
danielkl3 <danielkl@cyber.org.il>
2025-04-21 20:25:39 +0300

    Minor style changes

--- finance-app/src/main/java/com/kletinich/App.java
+++ finance-app/src/main/java/com/kletinich/App.java
@@ -19,7 +19,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("MainWindow"), 700, 500); 
+        scene = new Scene(loadFXML("MainWindow"), 840, 500); 
         stage.setScene(scene);
         stage.show();
     }
